0631a5ac01755c6099b955c64efc659599ce1a04
Lucas Bulhões <84589124+LOLB-dev98@users.noreply.github.com>
2021-05-24 17:07:18 -0300

    Create Atividade4.12.1.java
    Esta atividade exige a modelagem de uma conta bancária com os seguintes detalhamentos:
    
    - Número da conta;
    - Titular da conta;
    - Data de criação da conta;
    - Agência da conta;
    - Saldo da conta.
    
    E as seguintes operações devem ser possíveis:
    
    - Saque;
    - Deposito;
    - Rendimento Mensal.

---
+++ Atividade4.12.1.java
@@ -0,0 +1,34 @@
+class ContaB{
+	int numero;
+	Cliente titular;
+	String dataAbertura;
+	String agencia;
+	double saldo;
+	
+	boolean saca(double quantidade){
+		if(saldo < quantidade){
+			System.out.println("Saldo insuficiente! Tente novamente mais tarde");
+			return false;
+		}
+		else{
+			saldo -= quantidade;
+			System.out.println("Você sacou" +quantidade + "R$");
+			return true;
+		}
+		
+	}
+	
+	void deposita(double quantidade){
+		saldo +=quantidade;
+	}
+	
+	void rendimento(){
+		System.out,println("O rendimento mensal da conta eh:" + saldo*0.1 + "R$");
+	}
+}
+
+class Cliente{
+	String nome;
+	String sobrenome
+	String cpf
+}
